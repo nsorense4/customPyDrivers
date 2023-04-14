3731074ea81148f0f00f7c81a54b316a9c516ad3
Service Account Plank <srv_Plank@uottawa.o.univ>
2023-04-14 18:14:57 -0400

    Made changes to powerMonitor to add in DAQ.

--- powerMonitor.py
+++ powerMonitor.py
@@ -15,7 +15,7 @@ from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib import style
 import numpy as np
-from scipy.ndimage.interpolation import shift
+import nidaqmx
 
 import tkinter as tk
 import sys
@@ -38,16 +38,60 @@ N = 100
 timeInt = 0.1
 startTime = time.time()
 powerList = np.zeros(N)
+powerList0 = np.zeros(N)
 timeList = np.linspace(-N*timeInt, 0, N)
 
-PD = PM100D()
+configPM100D = 0
+configDAQ = 1
+config = configDAQ
+
+initDAQ = False
+
+def initDAQPD(avg = 10):
+    DAQ = nidaqmx.Task("task")
+    DAQ.ai_channels.add_ai_voltage_chan("Dev1/ai0", min_val=0, max_val=10)
+    DAQ.ai_channels.add_ai_voltage_chan("Dev1/ai1", min_val=0, max_val=10)
+    
+    return DAQ
+
+if config == configPM100D:
+    PD = PM100D()
+elif config == configDAQ:
+    if initDAQ == True:
+        DAQ = initDAQPD()
+    
+def convV2Int(volt):
+    # scatters for first order: 2.505 V, 1.345 mW, 2.82900037e-06 V, 0 mW
+    # zero order: 2.27825 V, 1.236 mW,  6.47855904e-06 V, 0 mW
+    slope = np.array([1.345/(2.505-0.01197444), 1.236/(2.27825-0.00533462)])
+    intercept = slope*np.array([0.01197444, (0.00533462)])
+    return slope*volt - intercept
+    
+def readPD(DAQ, PD = '0th', avgNum = 10):
+    # scale = np.array([1.236/2.27825, 1.345/2.505]) # mW/V
+    volt = np.mean(np.asarray(DAQ.read(number_of_samples_per_channel=avgNum)), axis = 1)
+    intensity = convV2Int(volt) #/ 1000
+
+    return intensity
+
 
 def animate(i):
-    global powerList, timeList
-    powerList = shift(powerList, -1, cval = PD.readPower()*1000)
+    global powerList, timeList, powerList0
+    if config == configPM100D:
+        powerList = shift(powerList, -1, cval = PD.readPower()*1000)
+    elif config == configDAQ:
+        power = readPD(DAQ)
+        powerList = shift(powerList, -1, cval = power[0])
+        powerList0 = shift(powerList0, -1, cval = power[1])
+    
     timeList = shift(timeList, -1, cval = time.time()-startTime)
     a.clear()
-    a.plot(timeList, powerList)
+    if config == configPM100D:
+        a.plot(timeList, powerList)
+    elif config == configDAQ:
+        a.plot(timeList, powerList)
+        a.plot(timeList, powerList0)
+    
     a.set_xlabel('Time (s)')
     a.set_ylabel('Power (mW)')
     f.tight_layout()
